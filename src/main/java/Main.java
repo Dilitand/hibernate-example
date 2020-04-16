ecd1d86728be09b199a27c8f931bf3e671c69d0b
Dmitriy <Dilitand@gmail.com>
2020-04-16 13:32:10 +0300

    add gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-
+        //spring
         Dao dao = new Dao();
         Personx person = new Personx("dimas");
 
